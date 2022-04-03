04b03b58b2e8920018799d883532b759183dda7c
Skwal <support@skwal.net>
2022-04-03 07:23:18 +0200

    add navbar (#2)

--- src/index.php
+++ src/index.php
@@ -1,1 +1,56 @@
-test
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SkwalExe</title>
+    <link rel="icon" type="image/ico" href="/favicon.ico">
+    <meta name="description" content="Hello 👋 I'm Léopold Koprivnivnik Ibghy, aka SkwalExe. I'm a 14 y/o French 🇫🇷 programming 💻 and Linux 🐧 lover. I use GitHub everyday since 2022/02/12. I code in rust 🦀, bash 🐚, and web languages 🌐. I also love making online courses.">
+    <meta property="og:title" content="SkwalExe" />
+    <meta property="og:image" content="/assets/banner.png" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta property="og:url" content="/" />
+    <meta property="og:site_name" content="© 2018-2022, Léopold Koprivnik Ibghy" />
+    <meta property="og:description" content="Hello 👋 I'm Léopold Koprivnivnik Ibghy, aka SkwalExe. I'm a 14 y/o French 🇫🇷 programming 💻 and Linux 🐧 lover. I use GitHub everyday since 2022/02/12. I code in rust 🦀, bash 🐚, and web languages 🌐. I also love making online courses." />
+    <meta name="theme-color" content="#FF514A">
+
+    <link rel="stylesheet" href="css/navbar.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/colors.css">
+</head>
+
+<body>
+
+    <div class="navbar">
+        <div class="container">
+
+            <div class="nav-top">
+                <div _href="https://github.com/SkwalExe/" class="logo">
+                    <img src="assets/github.png" alt="">
+                    <p class="text"><span class="purp">Skwal</span><span>Exe</span></p>
+                </div>
+                <div class="menu" id="toggleButton">
+                    <div class="menu-line"></div>
+                    <div class="menu-line"></div>
+                    <div class="menu-line"></div>
+                </div>
+
+            </div>
+            <ul id="navList">
+                <li href="https://github.com/SkwalExe/"><img src="/assets/github.png">Github</li>
+            </ul>
+        </div>
+
+
+    </div>
+
+
+    <script src="js/functions.js "></script>
+    <script src="js/navbar.js "></script>
+    <script src="js/links.js "></script>
+
+
+</body>
+
+</html>
