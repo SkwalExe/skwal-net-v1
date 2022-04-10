51700d0ecb9f17afd4d78c858b775f730682d4e9
SkwalExe <skwal.net@gmail.com>
2022-04-10 03:49:50 +0000

    Update production to output generated at 8b9fcd6

--- pageTemplate.php
+++ pageTemplate.php
@@ -27,63 +27,181 @@ include("{$_SERVER['DOCUMENT_ROOT']}/php/global.php");
     ?>
 
     <div class="main">
-        <div class="content box glowing">
+        <div class="content">
 
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Donec eleifend mauris id augue tempor, ac elementum arcu iaculis.
-                Curabitur lobortis mauris eget eros fringilla pharetra.
-                Integer tempus turpis lacinia, ornare lorem quis, placerat metus.
-                Aenean at orci vehicula, sagittis ante vitae, dapibus magna.
-                Vestibulum sed metus in mauris lobortis ultricies id sed enim.
-                Morbi viverra augue eget purus elementum, at rhoncus libero interdum.
-                Quisque fringilla tortor ut egestas laoreet.
-                Mauris et magna id tortor lobortis varius.
-                Pellentesque rhoncus magna ac lacus fringilla scelerisque.
-                Vivamus faucibus lacus ac mi sollicitudin, sed lacinia augue fringilla.
-                Proin posuere erat non molestie vulputate.
-                In tristique magna nec urna luctus fermentum.
-                Vivamus condimentum nibh at scelerisque cursus.
-                Duis elementum nibh sed leo pretium vehicula.
-                Curabitur porttitor felis gravida sagittis pharetra.
-                Nullam sed urna dignissim, sodales velit vitae, fringilla sapien.
-                Pellentesque in lorem consequat, lobortis est in, fermentum leo.
-                Maecenas auctor tellus vitae volutpat sodales.
-                Ut interdum eros dapibus, porttitor velit at, dapibus sapien.
-                Vestibulum tincidunt orci tristique nibh aliquet vehicula.
-                Proin varius est nec sapien vestibulum lobortis.
-                Proin interdum turpis dignissim sagittis sollicitudin.
-                Fusce eu turpis sit amet libero porta rutrum.
-                Integer pulvinar dolor a aliquet maximus.
-                Mauris vitae quam id leo iaculis sagittis.
-                Nullam eu enim sed elit tempor porttitor sed in turpis.
-                Proin non massa eu leo efficitur congue sit amet ut ante.
-                Aliquam eleifend ipsum ut ornare mollis.
-                Nunc dictum nunc fermentum nulla pellentesque porta.
-                Quisque ultrices est in bibendum rutrum.
-                Sed ac mi tempus, accumsan ante vitae, dignissim nisi.
-                Donec id mi vitae ex fermentum elementum.
-                Duis vel tortor sit amet purus elementum pellentesque.
-                Quisque at ipsum at quam egestas egestas eget in orci.
-                Maecenas tincidunt tortor vitae blandit commodo.
-                In efficitur justo in metus aliquet tristique.
-                Maecenas imperdiet ligula eget nunc sagittis, eu luctus ligula tincidunt.
-                Vestibulum commodo ligula vitae tincidunt sollicitudin.
-                Pellentesque sed purus sit amet augue pulvinar commodo sed ac justo.
-                Nunc vel tortor et nibh rutrum posuere vitae vitae magna.
-                Mauris quis tellus non ipsum efficitur luctus et ac turpis.
-                Aenean ut sapien quis magna ultrices accumsan sed et nisi.
-                Fusce et libero sit amet nisl euismod dictum sit amet in eros.
-                Aenean a nisi vitae nisi suscipit tincidunt eget at erat.
-                Donec nec tellus in velit ultricies mattis sit amet non sapien.
-                Quisque a mi venenatis nisl blandit malesuada porta ac nulla.
-                Vivamus porttitor elit at viverra lobortis.
-                Duis pulvinar arcu et arcu cursus, a sagittis elit vestibulum.
-                Vivamus maximus purus vitae dignissim faucibus.
-                Fusce eget mauris cursus, tempus diam quis, consequat sem.
-                Nam malesuada neque nec porttitor tempus.
+            <div class="box glowing">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Donec eleifend mauris id augue tempor, ac elementum arcu iaculis.
+                    Curabitur lobortis mauris eget eros fringilla pharetra.
+                    Integer tempus turpis lacinia, ornare lorem quis, placerat metus.
+                    Aenean at orci vehicula, sagittis ante vitae, dapibus magna.
+                    Vestibulum sed metus in mauris lobortis ultricies id sed enim.
+                    Morbi viverra augue eget purus elementum, at rhoncus libero interdum.
+                    Quisque fringilla tortor ut egestas laoreet.
+                    Mauris et magna id tortor lobortis varius.
+                    Pellentesque rhoncus magna ac lacus fringilla scelerisque.
+                    Vivamus faucibus lacus ac mi sollicitudin, sed lacinia augue fringilla.
+                    Proin posuere erat non molestie vulputate.
+                    In tristique magna nec urna luctus fermentum.
+                    Vivamus condimentum nibh at scelerisque cursus.
+                    Duis elementum nibh sed leo pretium vehicula.
+                    Curabitur porttitor felis gravida sagittis pharetra.
+                    Nullam sed urna dignissim, sodales velit vitae, fringilla sapien.
+                    Pellentesque in lorem consequat, lobortis est in, fermentum leo.
+                    Maecenas auctor tellus vitae volutpat sodales.
+                    Ut interdum eros dapibus, porttitor velit at, dapibus sapien.
+                    Vestibulum tincidunt orci tristique nibh aliquet vehicula.
+                    Proin varius est nec sapien vestibulum lobortis.
+                    Proin interdum turpis dignissim sagittis sollicitudin.
+                    Fusce eu turpis sit amet libero porta rutrum.
+                    Integer pulvinar dolor a aliquet maximus.
+                    Mauris vitae quam id leo iaculis sagittis.
+                    Nullam eu enim sed elit tempor porttitor sed in turpis.
+                    Proin non massa eu leo efficitur congue sit amet ut ante.
+                    Aliquam eleifend ipsum ut ornare mollis.
+                    Nunc dictum nunc fermentum nulla pellentesque porta.
+                    Quisque ultrices est in bibendum rutrum.
+                    Sed ac mi tempus, accumsan ante vitae, dignissim nisi.
+                    Donec id mi vitae ex fermentum elementum.
+                    Duis vel tortor sit amet purus elementum pellentesque.
+                    Quisque at ipsum at quam egestas egestas eget in orci.
+                    Maecenas tincidunt tortor vitae blandit commodo.
+                    In efficitur justo in metus aliquet tristique.
+                    Maecenas imperdiet ligula eget nunc sagittis, eu luctus ligula tincidunt.
+                    Vestibulum commodo ligula vitae tincidunt sollicitudin.
+                    Pellentesque sed purus sit amet augue pulvinar commodo sed ac justo.
+                    Nunc vel tortor et nibh rutrum posuere vitae vitae magna.
+                    Mauris quis tellus non ipsum efficitur luctus et ac turpis.
+                    Aenean ut sapien quis magna ultrices accumsan sed et nisi.
+                    Fusce et libero sit amet nisl euismod dictum sit amet in eros.
+                    Aenean a nisi vitae nisi suscipit tincidunt eget at erat.
+                    Donec nec tellus in velit ultricies mattis sit amet non sapien.
+                    Quisque a mi venenatis nisl blandit malesuada porta ac nulla.
+                    Vivamus porttitor elit at viverra lobortis.
+                    Duis pulvinar arcu et arcu cursus, a sagittis elit vestibulum.
+                    Vivamus maximus purus vitae dignissim faucibus.
+                    Fusce eget mauris cursus, tempus diam quis, consequat sem.
+                    Nam malesuada neque nec porttitor tempus.
 
-            </p>
+                </p>
+            </div>
+
+
+            <div class="box glowing">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Donec eleifend mauris id augue tempor, ac elementum arcu iaculis.
+                    Curabitur lobortis mauris eget eros fringilla pharetra.
+                    Integer tempus turpis lacinia, ornare lorem quis, placerat metus.
+                    Aenean at orci vehicula, sagittis ante vitae, dapibus magna.
+                    Vestibulum sed metus in mauris lobortis ultricies id sed enim.
+                    Morbi viverra augue eget purus elementum, at rhoncus libero interdum.
+                    Quisque fringilla tortor ut egestas laoreet.
+                    Mauris et magna id tortor lobortis varius.
+                    Pellentesque rhoncus magna ac lacus fringilla scelerisque.
+                    Vivamus faucibus lacus ac mi sollicitudin, sed lacinia augue fringilla.
+                    Proin posuere erat non molestie vulputate.
+                    In tristique magna nec urna luctus fermentum.
+                    Vivamus condimentum nibh at scelerisque cursus.
+                    Duis elementum nibh sed leo pretium vehicula.
+                    Curabitur porttitor felis gravida sagittis pharetra.
+                    Nullam sed urna dignissim, sodales velit vitae, fringilla sapien.
+                    Pellentesque in lorem consequat, lobortis est in, fermentum leo.
+                    Maecenas auctor tellus vitae volutpat sodales.
+                    Ut interdum eros dapibus, porttitor velit at, dapibus sapien.
+                    Vestibulum tincidunt orci tristique nibh aliquet vehicula.
+                    Proin varius est nec sapien vestibulum lobortis.
+                    Proin interdum turpis dignissim sagittis sollicitudin.
+                    Fusce eu turpis sit amet libero porta rutrum.
+                    Integer pulvinar dolor a aliquet maximus.
+                    Mauris vitae quam id leo iaculis sagittis.
+                    Nullam eu enim sed elit tempor porttitor sed in turpis.
+                    Proin non massa eu leo efficitur congue sit amet ut ante.
+                    Aliquam eleifend ipsum ut ornare mollis.
+                    Nunc dictum nunc fermentum nulla pellentesque porta.
+                    Quisque ultrices est in bibendum rutrum.
+                    Sed ac mi tempus, accumsan ante vitae, dignissim nisi.
+                    Donec id mi vitae ex fermentum elementum.
+                    Duis vel tortor sit amet purus elementum pellentesque.
+                    Quisque at ipsum at quam egestas egestas eget in orci.
+                    Maecenas tincidunt tortor vitae blandit commodo.
+                    In efficitur justo in metus aliquet tristique.
+                    Maecenas imperdiet ligula eget nunc sagittis, eu luctus ligula tincidunt.
+                    Vestibulum commodo ligula vitae tincidunt sollicitudin.
+                    Pellentesque sed purus sit amet augue pulvinar commodo sed ac justo.
+                    Nunc vel tortor et nibh rutrum posuere vitae vitae magna.
+                    Mauris quis tellus non ipsum efficitur luctus et ac turpis.
+                    Aenean ut sapien quis magna ultrices accumsan sed et nisi.
+                    Fusce et libero sit amet nisl euismod dictum sit amet in eros.
+                    Aenean a nisi vitae nisi suscipit tincidunt eget at erat.
+                    Donec nec tellus in velit ultricies mattis sit amet non sapien.
+                    Quisque a mi venenatis nisl blandit malesuada porta ac nulla.
+                    Vivamus porttitor elit at viverra lobortis.
+                    Duis pulvinar arcu et arcu cursus, a sagittis elit vestibulum.
+                    Vivamus maximus purus vitae dignissim faucibus.
+                    Fusce eget mauris cursus, tempus diam quis, consequat sem.
+                    Nam malesuada neque nec porttitor tempus.
+
+                </p>
+            </div>
+            <div class="box glowing">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Donec eleifend mauris id augue tempor, ac elementum arcu iaculis.
+                    Curabitur lobortis mauris eget eros fringilla pharetra.
+                    Integer tempus turpis lacinia, ornare lorem quis, placerat metus.
+                    Aenean at orci vehicula, sagittis ante vitae, dapibus magna.
+                    Vestibulum sed metus in mauris lobortis ultricies id sed enim.
+                    Morbi viverra augue eget purus elementum, at rhoncus libero interdum.
+                    Quisque fringilla tortor ut egestas laoreet.
+                    Mauris et magna id tortor lobortis varius.
+                    Pellentesque rhoncus magna ac lacus fringilla scelerisque.
+                    Vivamus faucibus lacus ac mi sollicitudin, sed lacinia augue fringilla.
+                    Proin posuere erat non molestie vulputate.
+                    In tristique magna nec urna luctus fermentum.
+                    Vivamus condimentum nibh at scelerisque cursus.
+                    Duis elementum nibh sed leo pretium vehicula.
+                    Curabitur porttitor felis gravida sagittis pharetra.
+                    Nullam sed urna dignissim, sodales velit vitae, fringilla sapien.
+                    Pellentesque in lorem consequat, lobortis est in, fermentum leo.
+                    Maecenas auctor tellus vitae volutpat sodales.
+                    Ut interdum eros dapibus, porttitor velit at, dapibus sapien.
+                    Vestibulum tincidunt orci tristique nibh aliquet vehicula.
+                    Proin varius est nec sapien vestibulum lobortis.
+                    Proin interdum turpis dignissim sagittis sollicitudin.
+                    Fusce eu turpis sit amet libero porta rutrum.
+                    Integer pulvinar dolor a aliquet maximus.
+                    Mauris vitae quam id leo iaculis sagittis.
+                    Nullam eu enim sed elit tempor porttitor sed in turpis.
+                    Proin non massa eu leo efficitur congue sit amet ut ante.
+                    Aliquam eleifend ipsum ut ornare mollis.
+                    Nunc dictum nunc fermentum nulla pellentesque porta.
+                    Quisque ultrices est in bibendum rutrum.
+                    Sed ac mi tempus, accumsan ante vitae, dignissim nisi.
+                    Donec id mi vitae ex fermentum elementum.
+                    Duis vel tortor sit amet purus elementum pellentesque.
+                    Quisque at ipsum at quam egestas egestas eget in orci.
+                    Maecenas tincidunt tortor vitae blandit commodo.
+                    In efficitur justo in metus aliquet tristique.
+                    Maecenas imperdiet ligula eget nunc sagittis, eu luctus ligula tincidunt.
+                    Vestibulum commodo ligula vitae tincidunt sollicitudin.
+                    Pellentesque sed purus sit amet augue pulvinar commodo sed ac justo.
+                    Nunc vel tortor et nibh rutrum posuere vitae vitae magna.
+                    Mauris quis tellus non ipsum efficitur luctus et ac turpis.
+                    Aenean ut sapien quis magna ultrices accumsan sed et nisi.
+                    Fusce et libero sit amet nisl euismod dictum sit amet in eros.
+                    Aenean a nisi vitae nisi suscipit tincidunt eget at erat.
+                    Donec nec tellus in velit ultricies mattis sit amet non sapien.
+                    Quisque a mi venenatis nisl blandit malesuada porta ac nulla.
+                    Vivamus porttitor elit at viverra lobortis.
+                    Duis pulvinar arcu et arcu cursus, a sagittis elit vestibulum.
+                    Vivamus maximus purus vitae dignissim faucibus.
+                    Fusce eget mauris cursus, tempus diam quis, consequat sem.
+                    Nam malesuada neque nec porttitor tempus.
+
+                </p>
+            </div>
         </div>
         <hr class="onlyShowWhenMobileWidth">
         <div class="sidebar">
