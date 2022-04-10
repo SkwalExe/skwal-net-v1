ec7a19eeeea318a95afb801007fb8bf53046edf7
Skwal <support@skwal.net>
2022-04-10 08:46:34 +0200

    fix 404 navbar logo (#27)

--- src/scripts/navbarStart.php
+++ src/scripts/navbarStart.php
@@ -3,7 +3,7 @@
 
         <div class="nav-top">
             <div _href="https://github.com/SkwalExe/" class="logo">
-                <img src="assets/github.png" alt="">
+                <img src="/assets/github.png" alt="">
                 <p class="text"><span class="purp">Skwal</span><span>Exe</span></p>
             </div>
             <div class="menu" id="toggleButton">
