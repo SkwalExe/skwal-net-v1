c1c31bd14897b177ca3b8e8dcc0a350dca88811b
SkwalExe <support@skwal.net>
2022-04-12 20:27:43 +0200

    bypass caching with javascript 'css'a and 'js' functions

--- src/php/variables.php
+++ src/php/variables.php
@@ -4,8 +4,12 @@ $root = $_SERVER['DOCUMENT_ROOT'];
 $ip = $_SERVER["REMOTE_ADDR"];
 $assets = "$root/assets";
 $scripts = "$root/scripts";
+
+$version = "69";
+
 $serverData = [
     "ip" => $ip,
+    "version" => $version,
 ];
-$version = "69";
+
 $bio = "Hello 👋 I'm Léopold Koprivnik Ibghy, aka SkwalExe. I'm a 14 y/o French 🇫🇷 programming 💻 and Linux 🐧 lover. I use GitHub everyday since 2022/02/12. I code in rust 🦀, bash 🐚, and web languages 🌐. I also love making online courses.";
