1739b06b342e17596d37f18fb5d229f11b4cf701
SkwalExe <support@skwal.net>
2022-05-21 07:26:54 +0200

    say "interesting file" when a file is dropped on the page

--- src/js/global.js
+++ src/js/global.js
@@ -5,3 +5,15 @@ $$(".sideBarTitle").forEach(element => {
   element.classList.add("box")
   element.classList.add("glowing")
 })
+
+document.body.ondragover = function(e) {
+  e.preventDefault()
+  return false;
+}
+document.body.ondrop = function(event) {
+  event.preventDefault()
+  if (!event.target.classList.contains('drop'))
+    if (event.dataTransfer.files.length > 0)
+      toasteur.success("Very interesting file!")
+
+};
