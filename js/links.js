2418ca5512f9cfabe9e6e02c105867fe3474d4e3
SkwalExe <skwal.net@gmail.com>
2022-06-05 04:47:47 +0000

    Update production to output generated at 5cc452c

--- js/links.js
+++ js/links.js
@@ -1,18 +1,21 @@
 $$("*[href], *[_href]").forEach(function(el) {
-    el.style.cursor = "pointer";
+  el.style.cursor = "pointer";
 });
 
 $$("*[href]").forEach(element => {
-    element.addEventListener("click", function(event) {
-        event.preventDefault();
-        window.location.href = this.getAttribute("href");
-    });
+  element.onclick = (e) => {
+    e.preventDefault();
+    if (e.ctrlKey)
+      window.open(this.getAttribute("href"), "_blank");
+    else
+      window.location.href = this.getAttribute("href");
+  };
 });
 
 $$("*[_href]").forEach(element => {
-    element.addEventListener("click", function(event) {
-        event.preventDefault();
+  element.addEventListener("click", function(event) {
+    event.preventDefault();
 
-        window.open(this.getAttribute("_href"), "_blank");
-    });
+    window.open(this.getAttribute("_href"), "_blank");
+  });
 });
