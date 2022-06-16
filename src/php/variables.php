c3ce5de42a15113cec622b7b4df0abf20606e916
SkwalExe <support@skwal.net>
2022-06-16 06:18:41 +0200

    add new page : morse translator

--- src/php/variables.php
+++ src/php/variables.php
@@ -5,7 +5,7 @@ $ip = $_SERVER["REMOTE_ADDR"];
 $assets = "$root/assets";
 $scripts = "$root/scripts";
 
-$version = "bdruh";
+$version = "bdrugyufih";
 
 $bio = "Hello 👋 I'm Léopold Koprivnik Ibghy, aka SkwalExe. I'm a 14 y/o French 🇫🇷 programming 💻 and Linux 🐧 lover. I use GitHub everyday since 2022/02/12. I code in rust 🦀, bash 🐚, and web languages 🌐. I also love making online courses.";
 
