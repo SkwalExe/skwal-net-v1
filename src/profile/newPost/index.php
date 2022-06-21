ea15d3eb3e90dd3e9a008dc7e062747585e35da1
SkwalExe <support@skwal.net>
2022-06-21 07:21:38 +0200

    new feature : editing posts

--- src/profile/newPost/index.php
+++ src/profile/newPost/index.php
@@ -1,6 +1,21 @@
 <?php
 include("{$_SERVER['DOCUMENT_ROOT']}/php/global.php");
-
+$error = true;
+$serverData['editPost'] = false;
+if (isLoggedIn()) {
+  $error = false;
+  if (requireGet("id")) {
+    $id = $_GET['id'];
+    $post = new Post($id);
+    if ($post->author_id == $_SESSION['id']) {
+      $error = false;
+      $serverData['editPost'] = true;
+    } else {
+      $error = true;
+      redirect("/", ['Error', 'You are not the author of the post you are trying to edit', "error"]);
+    }
+  }
+}
 ?>
 
 <!DOCTYPE html>
@@ -36,18 +51,20 @@ include("{$_SERVER['DOCUMENT_ROOT']}/php/global.php");
         <div class="box glowing">
           <form>
 
+            <?= $serverData['editPost'] ? "<input type='hidden' name='id' value='{$post->id}' >" : "" ?>
+
             <div class="input">
               <p class="inputLabel">
                 Title
               </p>
-              <input required placeholder="Title" type="text" name="title">
+              <input required placeholder="Title" type="text" name="title" <?= $serverData["editPost"] ? "value='{$post->title}'" : "" ?>>
             </div>
 
             <div class="input">
               <p class="inputLabel">
                 Content
               </p>
-              <textarea style="resize: vertical;" placeholder="Content" name="content" required></textarea>
+              <textarea style="resize: vertical;" placeholder="Content" name="content" required><?= $serverData["editPost"] ? $post->content : "" ?></textarea>
             </div>
 
             <button type="submit">Post</button>
