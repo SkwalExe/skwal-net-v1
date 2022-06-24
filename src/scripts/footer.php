895dcc21dc780f7fa302811407431c6c04682e68
SkwalExe <support@skwal.net>
2022-06-24 09:30:54 +0200

    add link to about page in the footer (#197)

--- src/scripts/footer.php
+++ src/scripts/footer.php
@@ -1,5 +1,8 @@
 <div class="noSelect footer">
     © 2018-<?= date('Y') ?>, Léopold Koprivnik Ibghy, all rights reserved
     <br>This website is open source and is available on <a href="https://github.com/SkwalExe/skwal.net">GitHub</a>
-    <br><a href="/cookies">🍪 Cookies policy</a> - <a href="/privacy">🔒 Privacy policy</a> - <a href="/tos">📜 Terms of Service</a>
+    <br>
+    <div class="flex center wrap" style="gap: 10px">
+        <a href="/about">🤔 About us</a><a href="/cookies">🍪 Cookies policy</a><a href="/privacy">🔒 Privacy policy</a><a href="/tos">📜 Terms of Service</a>
+    </div>
 </div>
